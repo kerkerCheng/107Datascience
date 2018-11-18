9583b888a3ef1ca325668c0877d05dc56e2b3660
kerkercheng <c3035281@gmail.com>
2018-11-19 00:25:08 +0800

    ready to submit

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -4,6 +4,7 @@ import datetime
 import time
 import pandas as pd
 import os
+import sys
 from sklearn.model_selection import cross_validate
 from sklearn import svm
 from sklearn.ensemble import AdaBoostClassifier, VotingClassifier
@@ -13,7 +14,7 @@ from sklearn.feature_selection import RFE
 
 
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%m.%d_%H.%M')
-mask = [2, 5, 7, 9]
+mask = [13]
 
 
 def read_file(train_path='train.csv', test_path='test.csv'):
@@ -62,22 +63,26 @@ def grid_search(train_X, y):
     classifier_xgb = xgb.XGBClassifier()
     classifier_svm = svm.SVC()
     classifier_ada = AdaBoostClassifier()
+    classifier_usvm = svm.NuSVC()
 
-    # parameters = {'C': [1.6, 1.65, 1.7, 1.75, 1.8, 1.85, 1.9, 1.95, 2.0], 'kernel': ['rbf']}
+    parameters = {'C': [1.6, 1.65, 1.7, 1.75, 1.8, 1.85, 1.9, 1.95, 2.0], 'kernel': ['rbf']}
     # parameters = {'n_estimators': [160, 180, 200, 220, 240, 260, 280], 'learning_rate': [1.5, 1.7, 1.9, 2.1, 2.3]}
-    parameters = {'max_depth': [6, 8, 10, 12],
-                  'learning_rate': [0.08, 0.1, 0.12],
-                  'n_estimators': [180, 210, 240],
-                  'n_jobs': [4],
-                  'objective': ['reg:logistic', 'reg:linear', 'binary:hinge'],
-                  'max_delta_step': [0, 2, 4, 6, 8],
-                  'min_child_weight': [1, 3, 5, 7, 9],
-                  'gamma': [0, 2, 4, 6]}
+    # parameters = {'max_depth': [6, 8, 10, 12],
+    #               'learning_rate': [0.08, 0.1, 0.12],
+    #               'n_estimators': [180, 210, 240],
+    #               'n_jobs': [4],
+    #               'objective': ['reg:logistic', 'reg:linear', 'binary:hinge'],
+    #               'max_delta_step': [0, 2, 4, 6, 8],
+    #               'min_child_weight': [1, 3, 5, 7, 9],
+    #               'gamma': [0, 2, 4, 6]}
+    # parameters = {'nu': [0.3, 0.5, 0.7],
+    #               'degree': [3, 5, 7]}
+
     num_splits = 3
-    clf_grid = GridSearchCV(classifier_xgb, parameters, cv=num_splits, n_jobs=-1, verbose=2)
+    clf_grid = GridSearchCV(classifier_svm, parameters, cv=num_splits, n_jobs=4, verbose=2)
     clf_grid.fit(train_X, y)
     grid_result = pd.DataFrame.from_dict(clf_grid.cv_results_)
-    grid_result.to_csv(timestamp + '_ada_grid.csv', sep=',', encoding='utf-8')
+    grid_result.to_csv(timestamp + '_svm_grid.csv', sep=',', encoding='utf-8')
 
     # cv_results = cross_validate(classifier_xgb, train_X, y, cv=num_splits, return_train_score=False)
     # cv_results = cross_validate(classifier_ada, train_X, y, cv=num_splits, return_train_score=False)
@@ -97,31 +102,32 @@ def rfecv(train_X, y):
 
 
 def main():
-    train_X, test_X, y = read_file()
+    train = sys.argv[1]
+    test = sys.argv[2]
+    train_X, test_X, y = read_file(train, test)
 
     # Training
     classifier_xgb = xgb.XGBClassifier(silent=True,
-                                       max_depth=4,
+                                       max_depth=6,
                                        learning_rate=0.1,
                                        n_estimators=210,
-                                       n_jobs=4,
                                        objective='reg:logistic')
     classifier_svm = svm.SVC(C=1.95)
     classifier_ada = AdaBoostClassifier(n_estimators=260, learning_rate=1.5)
 
-    grid_search(train_X, y)
+    # grid_search(train_X, y)
     # rfecv(train_X, y)
 
     # Prediction
-    # voting_models = [('xgb', classifier_xgb), ('svm', classifier_svm), ('ada', classifier_ada)]
-    # clf = VotingClassifier(estimators=voting_models, n_jobs=-1)
-    # clf.fit(train_X, y)
-    # ans = clf.predict(test_X)
-    #
-    # with open(os.path.abspath('sub_' + timestamp + '.csv'), 'w+') as f:
-    #     f.write('ID,ans\n')
-    #     for index, element in enumerate(ans):
-    #         f.write(str(index) + ',' + str(element) + '\n')
+    voting_models = [('xgb', classifier_xgb), ('svm', classifier_svm), ('ada', classifier_ada)]
+    clf = VotingClassifier(estimators=voting_models, n_jobs=None)
+    clf.fit(train_X, y)
+    ans = clf.predict(test_X)
+
+    with open(os.path.abspath('answer.csv'), 'w+') as f:
+        f.write('ID,ans\n')
+        for index, element in enumerate(ans):
+            f.write(str(index) + ',' + str(element) + '\n')
 
 
 if __name__ == '__main__':
