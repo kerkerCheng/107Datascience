d8efcd24c024e733cc995cd2cc5baa6cf2239f78
kerkercheng <c3035281@gmail.com>
2018-12-05 11:07:38 +0800

    ready to train

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import hw4_model as md
 from sklearn.model_selection import train_test_split
-# from keras.utils import np_utils
-# import model as md
-# from keras.callbacks import TensorBoard, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
-# from keras.preprocessing import image
+from keras.utils import np_utils
+from keras.callbacks import TensorBoard, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from keras.preprocessing import image
 import time
 import datetime
 import os
@@ -26,19 +26,20 @@ for i in range(0, csv_arr.shape[0]):
     X[i][:] = csv_arr[i][1:]
     y[i] = csv_arr[i][0]
 
-X = X.reshape((csv_arr.shape[0], 28, 28))
+X = X.reshape((csv_arr.shape[0], 28, 28, 1))
+y = np_utils.to_categorical(y)
 
 # Split Train and Validation #
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.05, random_state=42)
 
 # Parameters #
 num_epo = 10000
-num_batch = 32
-patience = 50
+num_batch = 64
+patience = 15
 verbose = 2
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d_%H.%M')
-log_file_path = 'logs/'+timestamp
-model_names = 'predict_model.hdf5'
+os.makedirs('./logs/'+timestamp)
+model_names = 'model_last_' + timestamp + '.hdf5'
 
 # Image augmentation #
 data_gen = image.ImageDataGenerator(rotation_range=39,
@@ -48,29 +49,25 @@ data_gen = image.ImageDataGenerator(rotation_range=39,
 data_gen.fit(X_train, augment=True, seed=1)
 img_generator = data_gen.flow(X_train, y_train, batch_size=num_batch)
 
-
 # Callbacks #
-# early_stop = EarlyStopping(monitor='val_acc', patience=patience)
-# tsb = TensorBoard(log_dir=log_file_path, histogram_freq=0, write_graph=True, write_images=True)
-# hist = md.LossHistory()
-# model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', save_best_only=True, verbose=1)
-# reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=int(patience/4), verbose=1)
-# call_back = [hist, tsb, early_stop, model_checkpoint, reduce_lr]
+early_stop = EarlyStopping(monitor='val_acc', patience=patience)
+hist = md.LossHistory()
+model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', save_best_only=True, verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.75, patience=4, verbose=1)
+call_back = [hist, early_stop, model_checkpoint, reduce_lr]
 
 # Train #
-# classifier = md.build()
-# classifier.fit_generator(img_generator,
-#                          steps_per_epoch=cut/num_batch,
-#                          epochs=num_epo,
-#                          verbose=verbose,
-#                          validation_data=(X_validation, Y_validation),
-#                          callbacks=call_back)
+classifier = md.build()
+classifier.fit_generator(img_generator,
+                         epochs=num_epo,
+                         verbose=verbose,
+                         validation_data=(X_val, y_val),
+                         callbacks=call_back)
 
-# tsb.set_model(classifier)
-# classifier.save(model_names)
-# md.output_history(hist, timestamp)
-# md.plot_acc(hist)
+classifier.save(model_names)
+md.output_history(hist, timestamp)
+md.plot_acc(hist)
 
-#
+
 # if __name__ == "__main__":
 #     main()
--- hw4/hw4_model.py
+++ hw4/hw4_model.py
@@ -4,64 +4,67 @@ from keras.layers import Dense, Dropout, Flatten, Conv2D, BatchNormalization, Ac
 from keras.layers import AveragePooling2D, GlobalAveragePooling2D, MaxPooling2D, SeparableConv2D, Add
 from keras.models import Model
 from keras.optimizers import Adam, SGD
+from keras.applications import inception_resnet_v2
 from keras import regularizers
 from keras.initializers import glorot_normal
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def build(l2_regularization=0.01, number_of_classes=7):
-    model = Sequential()
+    inputs = Input(shape=(28, 28, 1))
     regulizer = regularizers.l2(l2_regularization)
 
     # CNN part
-    model.add(Conv2D(filters=16, kernel_size=(3, 3), input_shape=(48, 48, 1), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=16, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=16, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(0.35))
+    outputs = Conv2D(filters=8, kernel_size=(3, 3), padding='same')(inputs)
+    outputs = Conv2D(filters=8, kernel_size=(3, 3), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
 
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(0.35))
+    outputs = Conv2D(filters=8, kernel_size=(3, 3), padding='same')(outputs)
+    outputs = Conv2D(filters=8, kernel_size=(3, 3), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
+    outputs = MaxPooling2D(pool_size=(2, 2), padding='same')(outputs)
 
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(0.35))
+    #
 
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(0.35))
+    outputs = Conv2D(filters=16, kernel_size=(4, 4), padding='same')(outputs)
+    outputs = Conv2D(filters=16, kernel_size=(4, 4), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
 
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(0.35))
+    outputs = Conv2D(filters=16, kernel_size=(4, 4), padding='same')(outputs)
+    outputs = Conv2D(filters=16, kernel_size=(4, 4), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
+    outputs = MaxPooling2D(pool_size=(2, 2), padding='same')(outputs)
 
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_uniform'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=number_of_classes, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(GlobalAveragePooling2D())
-    model.add(Activation('softmax'))
+    #
+
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
+
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
+
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Conv2D(filters=32, kernel_size=(5, 5), padding='same')(outputs)
+    outputs = Activation('selu')(outputs)
+    outputs = BatchNormalization()(outputs)
+    outputs = MaxPooling2D(pool_size=(2, 2), padding='same')(outputs)
+
+    outputs = Flatten()(outputs)
+
+    outputs = Dense(256, activation='relu')(outputs)
+    outputs = Dense(64, activation='relu')(outputs)
+    outputs = Dense(10, activation='softmax')(outputs)
+
+    model = Model(inputs=inputs, outputs=outputs)
 
     opt = Adam(0.01)
     model.compile(loss='categorical_crossentropy',
@@ -73,27 +76,27 @@ def build(l2_regularization=0.01, number_of_classes=7):
     return model
 
 
-def output_history(his, time):
-    with open('./logs/' + time + '/train_loss', 'w+') as f:
+def output_history(his, timestamp):
+    with open('./logs/' + timestamp + '/train_loss', 'w+') as f:
         f.writelines("%f\n" % i for i in his.train_loss)
-    with open('./logs/' + time + '/validation_loss', 'w+') as f:
+    with open('./logs/' + timestamp + '/validation_loss', 'w+') as f:
         f.writelines("%f\n" % i for i in his.validation_loss)
-    with open('./logs/' + time + '/train_acc', 'w+') as f:
+    with open('./logs/' + timestamp + '/train_acc', 'w+') as f:
         f.writelines("%f\n" % i for i in his.train_acc)
-    with open('./logs/' + time + '/validation_acc', 'w+') as f:
+    with open('./logs/' + timestamp + '/validation_acc', 'w+') as f:
         f.writelines("%f\n" % i for i in his.validation_acc)
 
 
-# def plot_acc(his):
-#     x = np.arange(0, len(his.train_acc))
-#     y1 = his.train_acc
-#     y2 = his.validation_acc
-#     plt.plot(x, y1)
-#     plt.plot(x, y2)
-#     plt.legend([' training acc ', ' validation acc '], fontsize=12)
-#     plt.xlabel('num. of epochs', fontsize=12)
-#     plt.ylabel('acc. (%)', fontsize=12)
-#     plt.show()
+def plot_acc(his):
+    x = np.arange(0, len(his.train_acc))
+    y1 = his.train_acc
+    y2 = his.validation_acc
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.legend([' training acc ', ' validation acc '], fontsize=12)
+    plt.xlabel('num. of epochs', fontsize=12)
+    plt.ylabel('acc. (%)', fontsize=12)
+    plt.show()
 
 
 class LossHistory(Callback):
